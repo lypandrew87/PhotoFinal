ea077ce111bf23253ef9a361d89503dccb6086e4
lypand <andrew.lyp@chrobinson.com>
2018-01-16 23:28:56 -0600

    Adding carousel and parallex effect

--- src/app/photo-header/photo-header.component.ts
+++ src/app/photo-header/photo-header.component.ts
@@ -13,48 +13,10 @@ export class PhotoHeaderComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-
     parallaxIcon();
-    rotateBackgroundImage();
+    parallaxCarousel(); 
   }
-}
 
-function rotateBackgroundImage() {
-
-  var images = [
-    "../assets/header-background1.jpg",
-    "../assets/header-background2.jpg",
-    "../assets/header-background3.jpg",
-    "../assets/header-background4.jpg",
-    "../assets/header-background5.jpg",
-    "../assets/header-background6.jpg",
-    "../assets/header-background7.jpg",
-    "../assets/header-background8.jpg",
-    "../assets/header-background9.jpg",
-    "../assets/header-background10.jpg",
-    "../assets/header-background11.jpg",
-    "../assets/header-background12.jpg"
-  ]
-  images.forEach(function (img) {
-    new Image().src = img;
-
-
-    var imageHead = document.getElementById("id");
-
-    const cycleImages = (images, container, step) => {
-      images.forEach((image, index) => (
-        setTimeout(() => {
-          container.style.backgroundImage
-          container.style.backgroundImage = `url(${image})`
-          container.style.background
-        }, step * (index + 1))
-      ))
-      setTimeout(() => cycleImages(images, container, step), step * images.length)
-    }
-
-    cycleImages(images, imageHead, 9000)
-  }
-  )
 }
 
 function parallaxIcon() {
@@ -64,5 +26,12 @@ function parallaxIcon() {
       'transform': 'translate(0px, -' + wScroll / 100 + '%)'
     });
   });
-
+}
+function parallaxCarousel() {
+  $(window).scroll(function () {
+    var wScroll = $(this).scrollTop();
+    $('.carousel').css({
+      'transform': 'translate(0px, +' + wScroll / 100 + '%)'
+    });
+  });
 }
